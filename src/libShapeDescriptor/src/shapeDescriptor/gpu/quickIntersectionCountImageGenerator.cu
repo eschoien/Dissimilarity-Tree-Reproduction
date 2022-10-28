c430c916f48a213d680f503c5ca1cbe3ff677fb6
eschoien <eschoien@gmail.com>
2022-10-28 14:27:35 +0200

    Change quicci generator to vertical

--- src/libShapeDescriptor/src/shapeDescriptor/gpu/quickIntersectionCountImageGenerator.cu
+++ src/libShapeDescriptor/src/shapeDescriptor/gpu/quickIntersectionCountImageGenerator.cu
@@ -256,12 +256,14 @@ __device__ void writeQUICCImage(
     const int laneIndex = threadIdx.x % 32;
     static_assert(spinImageWidthPixels % 32 == 0, "Implementation assumes images are multiples of warps wide");
 
-    for(int row = 0; row < spinImageWidthPixels; row++) {
+    // col 0 turns into bottom row, should be leftmost col
+
+    for(int col = 0; col < spinImageWidthPixels; col++) {
         radialIntersectionCountImagePixelType previousWarpLastNeedlePixelValue = 0;
 
         for (int pixel = laneIndex; pixel < spinImageWidthPixels; pixel += warpSize) {
             radialIntersectionCountImagePixelType currentNeedlePixelValue =
-                    RICIDescriptor[row * spinImageWidthPixels + pixel];
+                    RICIDescriptor[pixel * spinImageWidthPixels + col];
 
             int targetThread;
             if (laneIndex > 0) {
@@ -290,7 +292,7 @@ __device__ void writeQUICCImage(
             unsigned int changeOccurredCombined = __brev(__ballot_sync(0xFFFFFFFF, didIntersectionCountsChange));
 
             if(laneIndex == 0) {
-                size_t chunkIndex = (row * (spinImageWidthPixels / 32)) + (pixel / 32);
+                size_t chunkIndex = (col * (spinImageWidthPixels / 32)) + (pixel / 32);
                 descriptorArray[blockIdx.x].contents[chunkIndex] = changeOccurredCombined;
             }
 
@@ -298,6 +300,24 @@ __device__ void writeQUICCImage(
             previousWarpLastNeedlePixelValue = currentNeedlePixelValue;
         }
 
+    }
+
+    __syncthreads();
+
+    if (threadIdx.x == 0) {
+    // transpose matrix
+
+        int j, k;
+        unsigned m, t;
+
+        m = 0x0000FFFF;
+        for (j = 16; j != 0; j = j >> 1, m = m ^ (m << j)) {
+            for (k = 0; k < 32; k = (k + j + 1) & ~j) {
+                t = (descriptorArray[blockIdx.x].contents[k] ^ (descriptorArray[blockIdx.x].contents[k+j] >> j)) & m;
+                descriptorArray[blockIdx.x].contents[k] = descriptorArray[blockIdx.x].contents[k] ^ t;
+                descriptorArray[blockIdx.x].contents[k+j] = descriptorArray[blockIdx.x].contents[k+j] ^ (t << j);
+            }
+        }
     }
 }
 
