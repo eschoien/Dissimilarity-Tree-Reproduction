3ef6a8ce8482ab68ffecd168f407834872e94d45
Eirik Schøien <eschoien@gmail.com>
2023-02-07 12:05:12 +0100

    Refactor minhash

--- src/partialRetrieval/src/projectSymmetry/lsh/Minhash.cpp
+++ src/partialRetrieval/src/projectSymmetry/lsh/Minhash.cpp
@@ -1,33 +1,30 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 #include <shapeDescriptor/utilities/fileutils.h>
 #include <shapeDescriptor/utilities/print/QuicciDescriptor.h>
 #include "Signature.h"
 #include "Permutation.h"
 
-// want to pass pointer instead of returning
-void computeDescriptorSignature(ShapeDescriptor::QUICCIDescriptor descriptor, std::vector<int>* signatures, std::vector<std::vector<int>> permutations) {
-// std::vector<int> computeDescriptorSignature(ShapeDescriptor::QUICCIDescriptor descriptor, std::vector<std::vector<int>> permutations) {
-
+void computeDescriptorSignature(ShapeDescriptor::QUICCIDescriptor descriptor, std::vector<int>* signaturePtr, std::vector<std::vector<int>> permutations) {
+    
     for (int p = 0; p < permutations.size(); p++) {
 
         std::vector<int> permutation = permutations[p];
-
-        // improve this condition to be based on spinImagePixels etc.
-        // perhaps change to for loop
-
         int m = 0;
+        bool quicciBit = false;
+        
+        // improve this condition to be based on spinImagePixels etc.
         while (m < 1024) {
-            // make this line more readable?
-            if (descriptor.contents[(permutation[m] / 32)] & (1 << (permutation[m] % 32)))
-            {
+            // Old one-line version: (descriptor.contents[(permutation[m] / 32)] & (1 << (permutation[m] % 32)))
+            unsigned int quicciRow = descriptor.contents[(permutation[m] / 32)];
+            unsigned int quicciCol = permutation[m] % 32;
+            quicciBit = quicciRow & (1 << quicciCol);
+            if (quicciBit) {
                 break;
             }
             m++;
         }
         // m is now the signature of this descriptor for the current permutation
-        signatures->push_back(m);
-        
+        signaturePtr->push_back(m);
     }
 }
