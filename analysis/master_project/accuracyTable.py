1af9c98e782c1f15bd956dc5557a0366b5763c83
Eirik Schøien <eschoien@gmail.com>
2023-03-14 12:51:12 +0100

    Refactor accuracy table

--- analysis/master_project/accuracyTable.py
+++ analysis/master_project/accuracyTable.py
@@ -1,8 +1,9 @@
 import json
 from scripts.prettytable import PrettyTable
 from datetime import timedelta
-
 # Run from DTR directory, will not find files if cd'ed into folder of this file
+
+
 def calculate_accuracy(data, k):
 
     correct_guesses = 0
@@ -15,6 +16,7 @@ def calculate_accuracy(data, k):
     accuracy = (correct_guesses / len(data['results'])) * 100
     return amount, accuracy
 
+
 def calculate_time(data):
     time = 0.0
     for query in data['results']:
@@ -25,84 +27,56 @@ def calculate_time(data):
     return time, avg_time
 
 
-
-
 # --- ARGUMENTS ---
-basePath = 'output/lsh/measurements/v2/'
+basePath = 'output/lsh/measurements/v3/'
 ks = [1, 3, 5, 10]
 permutations = [10, 50, 100]
 thresholds =  ["0.1","0.2","0.3","0.4","0.5","0.6","0.7","0.8","0.9","1.0"]
-limits = [100, 500, 1000, 0, 101, 501, 1001]
+querysets = ["partial", "complete"]
+limits = [100, 500, 1000]
 # -----------------
 
 
 for k in ks:
+
     for p in permutations:
-        try:
-            timeSec = 0.0
-            outputTable = PrettyTable(['Jaccard', "P100", "P500", "P1000", "-", "C100", "C500", "C1000"])
-            outputTable.align = "r"
 
-            accuracies = {}
-            times = {}
+        outputTable = PrettyTable()
+        outputTable.align = "r"
+        outputTable.add_column("Jaccard", thresholds+["Total time"])
+        totalTime = 0
 
-            for j in thresholds:
-                times[j] = {}
-                accuracies[j] = {}
-                partial = True
-                for d in limits:
-                    try:
-                        if d == 0:
-                            partial = False
+        for queryset in querysets:
 
-                        if partial:
-                            signature_data = json.load(open(basePath + 'partial_objects/permcount'+str(p)+'/measurement-'+j+'-'+str(d)+'-'+str(p)+'.json'))
-                        else:
-                            signature_data = json.load(open(basePath + 'complete_objects/permcount'+str(p)+'/measurement-'+j+'-'+str(d-1)+'-'+str(p)+'.json'))
+            outputTable.add_column("-", ["-"]*11,)
 
-                        # TIME
-                        time, avg_time = calculate_time(signature_data)
-                        times[j][d] = time
-                        timeSec += time
-
-                        # ACCURACY
-                        signature_accuracy = calculate_accuracy(signature_data, k)
-                        accuracies[j][d] = f'{round(signature_accuracy[1],2)}%'
-                       #accuracies[j][d] = f'{round(signature_accuracy[1],2)}%, {round(time, 3)}' # Also print avg execution time
-                       #accuracies[j][d] = f'{signature_accuracy[0]}' # Print number of correct results / number of queries
-
-                    except:
-                        accuracies[j][d] = "-"
-                
-                outputTable.add_row([j] + list(accuracies[j].values()))
-
-            ## ------------ ADD TIMES FOR EACH COLUMN -----------------------
-            total_times = []
             for d in limits:
-                
-                total_time = 0.0
-                
-                if d != 0:
-                    for j in thresholds:
-                        total_time += times[j][d]
-                    total_times.append(timedelta(seconds=round(total_time)))
-                else:
-                    total_times.append('-')
-            
-            outputTable.add_row(['Total time'] + total_times)
-            ## -------------------------------------------------------------
-            
 
-            #print("             {-----------Partial-----------}   {-----------Complete----------}")
-            print(f'Results for top-k={k}, Permutations p={p}, Total time: {timedelta(seconds=round(timeSec))}')
-            print(outputTable)
-            
-        except: # If the specified measurement files doesn't exist
+                columnAccuracy = []
+                columnTimes = []
 
-            print(f'Results for top-k={k}, Permutations p={p}, Total time: ...')
-            print(f'--- Missing data for Permutations p={p} ---')
-            print()
+                for j in thresholds:
 
-        finally: #Add spacing in both cases
-            print()
-            print()
+                    try:
+                        signature_data = json.load(open(basePath + queryset + '_objects/permcount'+str(p)+'/measurement-'+j+'-'+str(d)+'-'+str(p)+'.json'))
+
+                        signature_accuracy = calculate_accuracy(signature_data, k)
+                        columnAccuracy.append('{:.2f}%'.format(round(signature_accuracy[1],2)).rjust(7, " "))
+                        #columnAccuracy.append(f'{round(signature_accuracy[1],2)}%, {round(time, 3)}') # Also print avg execution time
+                        #columnAccuracy.append(f'{signature_accuracy[0]}') # Print number of correct results / number of queries
+
+                        time, avg_time = calculate_time(signature_data)
+                        columnTimes.append(time)
+                        totalTime += time
+                    
+                    except:
+                        columnAccuracy.append('-')
+                        columnTimes.append(0)
+                        
+                outputTable.add_column(queryset[0].upper()+str(d), columnAccuracy+[timedelta(seconds=round(sum(columnTimes)))])
+
+        print(str("{"+"Partial".center(29,"-")+"}   {"+"Complete".center(29,"-")+"}").rjust(82, " "))
+        print(outputTable)
+        print(f'Results for top-k={k}, Permutations p={p}, Total time: {timedelta(seconds=round(totalTime))}')
+        print()
+        print()
