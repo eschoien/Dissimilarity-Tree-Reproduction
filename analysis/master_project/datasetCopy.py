8c1f7462569e0791e502ff581b85a965676ce1fc
Oystein99 <oystein147@gmail.com>
2023-06-01 15:41:09 +0200

    Prepare for master

--- analysis/master_project/datasetCopy.py
+++
@@ -1,8 +0,0 @@
-import shutil
-
-for i in range(1, 101):
-
-    src_path = "ObjectFolder1-100/"+str(i)+"/model.obj"
-    dst_path = "input/MIT_CSAIL/complete/T"+str(i)+".obj"
-    shutil.copy(src_path, dst_path)
-print('Copied object', i)
